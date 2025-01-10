c7ffd45b6e593e158beacb0707523bd09295f1ef
MateuszCzz <mateuszczernic.mc@gmail.com>
2025-01-10 02:00:00 +0100

    Change driver to headless firefox

--- scraper/driver.py
+++ scraper/driver.py
@@ -1,8 +1,14 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
+def create_driver(headless: bool = True) -> Firefox:
+    options = Options()
+    if headless:
+        options.add_argument("--headless")
+    options.add_argument("--width=1920")
+    options.add_argument("--height=1080")
 
-def create_driver() -> Firefox:
     service = FirefoxService(GeckoDriverManager().install())
-    return Firefox(service=service)
+    return Firefox(service=service, options=options)
